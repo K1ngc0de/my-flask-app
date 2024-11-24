4211f0b88b08beedcb37474fd2940d9eb61a82cd
Vadim Advahov <advakhov97@mail.ru>
2024-11-24 12:47:04 +0200

    Changed heroku on vercel

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002) #commit
+    # Используем порт из переменной окружения PORT, если она есть, иначе 5000
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host="0.0.0.0", port=port)
+
